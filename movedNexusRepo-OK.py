b5b187e22d74a2a69ea99da4959a15b77397059e
reinaldoca <55362209+reinaldoca@users.noreply.github.com>
2023-02-12 16:46:40 -0300

    Create movedNexusRepo-OK.py

---
+++ movedNexusRepo-OK.py
@@ -0,0 +1,48 @@
+import requests, os, time
+
+#currentNexus="https://nexus"
+#anothernexus="https://anotherNexus"
+#repositoryName="repositoryName"
+
+url=f"{currentNexus}/service/rest/v1/components?repository={repositoryName}"
+
+start_time=time.time()
+nextToken="firstStart"
+path_list=[]
+
+
+while nextToken != None:
+    if nextToken == "firstStart":
+      response=requests.get(url)
+      result=response.json()
+    else:
+      response=requests.get(f"{url}&continuationToken={nextToken}")
+      result=response.json()
+
+    for y in result['items']:
+        for i in y['assets']:
+            download_url=i['downloadUrl']
+            upload_url=download_url.replace(f"{currentNexus}",f"{anotherNexus}")
+            file_name=download_url.split("/")[-1]
+
+
+            os.system(f"curl -X GET {download_url} > {file_name}")
+#            os.system(f"curl -v -k --username admin:admin123 --uplaod-file {file_name} {upload_url}")
+#            os.system(f"curl -v -k -u admin:admin123 --uplaod-file {file_name} {upload_url}")
+            os.system(f"curl -v -k -u admin:admin123 -T {file_name} {upload_url}")
+            os.system("rm -f {file_name}")
+
+            if i['path'] in path_list:
+                print("Already in the list")
+            else:
+                path_list.append(i['path'])
+
+nextToken=result['continuationToken']
+
+
+completed_time=time.time()
+completion_time=completed_time-start_time
+completion_time_minute=completion_time/60
+
+print(f"Completion Time : {completion_time_minute}")
+print(f"File Counts : {len(path_list)}")
